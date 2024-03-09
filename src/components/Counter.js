63a402ddd8e157666c94621869d67191162a5548
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-09 18:30:18 +0530

    useSelector (subscribing Component to store and getting slice from store)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,17 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector } from "react-redux";
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
 
+  // importing useSelector from react-redux it will subscribe this component to the store
+  // useSelector will give store slice means  data which want in this component
+  const counter = useSelector((state) => state.counter);
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
